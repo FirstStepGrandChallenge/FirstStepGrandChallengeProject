92f8184bac4f363e48c23938ea0490a904fb32a8
Pengzl1 <zhanglongpeng@wifi-roaming-128-4-190-229.host.udel.edu>
2017-01-25 16:47:29 -0500

    Initial Commit

---
+++ FirstStep Grand Challenge ProjectTests/FirstStep_Grand_Challenge_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  FirstStep_Grand_Challenge_ProjectTests.swift
+//  FirstStep Grand Challenge ProjectTests
+//
+//  Created by zhanglong Peng on 25/01/2017.
+//  Copyright © 2017 zhanglong Peng. All rights reserved.
+//
+
+import XCTest
+@testable import FirstStep_Grand_Challenge_Project
+
+class FirstStep_Grand_Challenge_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
