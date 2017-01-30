537de17c965a1ffa5c7802f8cfcec7d082f08ee3
Pengzl1 <zhanglongpeng@wifi-roaming-128-4-187-28.host.udel.edu>
2017-01-30 18:11:48 -0500

    sign in

--- FirstStep Grand Challenge Project/SignInViewController.swift
+++ FirstStep Grand Challenge Project/SignInViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import FirebaseAuth
 
 class SignInViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +24,46 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func SignInAction(_ sender: UIButton) {
+
+                if self.email.text == "" || self.password.text == "" {
+        
+                    //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+        
+                    let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+        
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+        
+                    self.present(alertController, animated: true, completion: nil)
+        
+                } else {
+        
+                    FIRAuth.auth()?.signIn(withEmail: self.email.text!, password: self.password.text!) { (user, error) in
+        
+                        if error == nil {
+        
+                            //Print into the console if successfully logged in
+                            print("You have successfully logged in")
+        
+                            //Go to the HomeViewController if the login is sucessful
+                            let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                            self.present(vc!, animated: true, completion: nil)
+        
+                        } else {
+        
+                            //Tells the user that there is an error and then gets firebase to tell them the error
+                            let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+        
+                            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            alertController.addAction(defaultAction)
+                            
+                            self.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                }
+
+    }
 
     /*
     // MARK: - Navigation
