5bc2752494aeb9a63920008ac7cba24872858e21
Pengzl1 <zhanglongpeng@wifi-roaming-128-4-187-28.host.udel.edu>
2017-01-30 18:55:43 -0500

    sign up

---
+++ FirstStep Grand Challenge Project/SignUpViewController.swift
@@ -0,0 +1,79 @@
+//
+//  SignUpViewController.swift
+//  FirstStep Grand Challenge Project
+//
+//  Created by zhanglong Peng on 30/01/2017.
+//  Copyright © 2017 zhanglong Peng. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password1: UITextField!
+    @IBOutlet weak var password2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignUp(_ sender: Any) {
+        if email.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            if password1.text == password2.text{
+                FIRAuth.auth()?.createUser(withEmail: email.text!, password: password1.text!) { (user, error) in
+                    
+                    if error == nil {
+                        print("You have successfully signed up")
+                        //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                        
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                        self.present(vc!, animated: true, completion: nil)
+                        
+                    } else {
+                        let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                        
+                        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(defaultAction)
+                        
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            }
+            else{
+                let alertController = UIAlertController(title: "Error", message: "password doesnt match", preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
