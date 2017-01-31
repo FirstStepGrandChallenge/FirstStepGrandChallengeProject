6d1d50284c434e362aff2865de9103076e241e00
Pengzl1 <zhanglongpeng@wifi-roaming-128-4-184-9.host.udel.edu>
2017-01-31 17:26:41 -0500

    password reset

--- FirstStep Grand Challenge Project/ForgetViewController.swift
+++ FirstStep Grand Challenge Project/ForgetViewController.swift
@@ -7,29 +7,78 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class ForgetViewController: UIViewController {
-
+    
+    @IBOutlet weak var email: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func ResetSubmitAction(_ sender: Any) {
+    
+        if self.email.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            FIRAuth.auth()?.sendPasswordReset(withEmail: self.email.text!, completion: {
+                (error) in
+                var title = ""
+                var message = ""
+                if error != nil{
+                    title = "oops"
+                    message = (error?.localizedDescription)!
+                }
+                else{
+                    title = "Success!"
+                    message = "Password reset email sent"
+                    self.email.text = ""
+                }
+                
+                
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+                
+                
+                
+            })
+        }
+        
+        
+        
+        
+        
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
