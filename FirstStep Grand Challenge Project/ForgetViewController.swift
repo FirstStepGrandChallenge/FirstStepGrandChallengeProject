cbbc0ef188b5d08c5c1add6ba5fb1a8c4097f628
lanchenn <allielc@udel.edu>
2017-01-31 16:59:02 -0500

    Jan 31 second commit by LAN

---
+++ FirstStep Grand Challenge Project/ForgetViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ForgetViewController.swift
+//  FirstStep Grand Challenge Project
+//
+//  Created by LAN CHEN on 1/31/17.
+//  Copyright © 2017 zhanglong Peng. All rights reserved.
+//
+
+import UIKit
+
+class ForgetViewController: NSObject {
+    
+
+}
---
+++ FirstStep Grand Challenge Project/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  FirstStep Grand Challenge Project
+//
+//  Created by LAN CHEN on 1/31/17.
+//  Copyright © 2017 zhanglong Peng. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: NSObject {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
