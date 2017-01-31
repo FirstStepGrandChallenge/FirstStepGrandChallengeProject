a8007e32f6333d44398d2940394b53d1e921c444
Pengzl1 <zhanglongpeng@wifi-roaming-128-4-184-9.host.udel.edu>
2017-01-31 17:05:54 -0500

    view controller

--- FirstStep Grand Challenge Project/ForgetViewController.swift
+++ FirstStep Grand Challenge Project/ForgetViewController.swift
@@ -2,13 +2,34 @@
 //  ForgetViewController.swift
 //  FirstStep Grand Challenge Project
 //
-//  Created by LAN CHEN on 1/31/17.
+//  Created by zhanglong Peng on 31/01/2017.
 //  Copyright © 2017 zhanglong Peng. All rights reserved.
 //
 
 import UIKit
 
-class ForgetViewController: NSObject {
+class ForgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
