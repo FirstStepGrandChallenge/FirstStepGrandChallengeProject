7076faf2da372364f1ab7d2a43024bb97da1bf62
Jessica Chen <JC@Jessicas-MacBook-Air.local>
2017-03-04 15:40:48 -0500

    registrationInfor update

--- FirstStep Grand Challenge Project/RegistrationInforViewController.swift
+++ FirstStep Grand Challenge Project/RegistrationInforViewController.swift
@@ -1,28 +1,16 @@
-//
-//  RegistrationInforViewController.swift
-//  FirstStep Grand Challenge Project
-//
-//  Created by Jessica Chen on 3/3/17.
-//  Copyright © 2017 zhanglong Peng. All rights reserved.
-//
-
 import UIKit
 
-class RegistrationInforViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class RegistrationInforViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    @IBOutlet weak var genderInput: UITextField!
-    @IBOutlet weak var genderPicker: UIPickerView!
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var dropDown: UIPickerView!
     
-    var Array = ["Female", "Male", "Other"]
-    var placementInput = 0
+    
+    var list = ["Female", "Male", "Other"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        
-        genderPicker.delegate = self;
-        genderPicker.dataSource = self;
+        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,46 +19,40 @@ class RegistrationInforViewController: UIViewController, UIPickerViewDelegate, U
     }
     
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+        
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        return list.count
+        
+    }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return Array[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return Array.count
-    }
-    
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    @IBAction func submit(_ sender: Any) {
-        if (placementInput == 0) {
-            genderInput.text = "Female"
-        }
-            
-        else if (placementInput == 1) {
-            genderInput.text = "Male"
-        }
-            
-        else {
-            genderInput.text = "Other"
-        }
+        
+        self.view.endEditing(true)
+        return list[row]
         
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        placementInput = row
+        
+        self.textBox.text = self.list[row]
+        self.dropDown.isHidden = true
+        
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField == self.textBox {
+            self.dropDown.isHidden = false
+            //if you dont want the users to se the keyboard type:
+            
+            textField.endEditing(true)
+        }
+        
+    }
 }
 
